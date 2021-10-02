61f5595020d4d77117c7b8e99eacac46d0e708f6
Alex <alex.talha@infracommerce.com.br>
2021-10-02 13:32:18 -0300

    Teste 1231232

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Teste!!!!!')
+})
+
+app.listen(3000, () =>
+    console.log('Example app listening on pfghghghhgort 3000!!!!!!!!!!!66!!!!')
+)
